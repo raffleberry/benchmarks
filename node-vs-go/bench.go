9bc9be6ccc4a8aa4678ef26ce944c0dbc6b45189
raffleberry <raffleberry42@gmail.com>
2022-08-23 01:16:03 +0530

    improvement: improved worker pool

--- node-vs-go/bench.go
+++ node-vs-go/bench.go
@@ -6,9 +6,10 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"sync"
 	"syscall"
 )
+
+type token struct{}
 
 func exit() {
 	fmt.Println("USAGE: bench.go <url> <parallel-connections>")
@@ -19,39 +20,12 @@ func metrics(endTime, execTime int64) {
 	// TODO: Implement circular buffer.
 }
 
-func work(url string) {
+func work(url string, tokens chan token) {
 	_, err := http.Get(url)
 	if err != nil {
 		// TODO: handle error
 	}
-}
-
-func worker(stopConsumerChan <-chan bool, jobChan <-chan string, w *sync.WaitGroup) {
-	w.Add(1)
-	fmt.Println(w)
-	count := 0
-	for {
-		select {
-		case url := <-jobChan:
-			count++
-			work(url)
-		case <-stopConsumerChan:
-			defer w.Done()
-			return
-		}
-	}
-}
-
-func generate(url string, stopChan <-chan os.Signal, jobChan chan string) {
-	for {
-		select {
-		case <-stopChan:
-			fmt.Println("Benchmark Stopping..")
-			return
-		default:
-			jobChan <- url
-		}
-	}
+	<-tokens
 }
 
 func main() {
@@ -60,35 +34,38 @@ func main() {
 	}
 
 	url := os.Args[1]
-	conn, err := strconv.Atoi(os.Args[2])
+	fmt.Println(url)
+	limit, err := strconv.Atoi(os.Args[2])
 
 	if err != nil {
 		exit()
 	}
 
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, os.Interrupt)
+
+	tokens := make(chan token, limit)
+
 	fmt.Println("Benchmark Start")
-
-	stopProducerChan := make(chan os.Signal, 1)
-	stopConsumerChan := make(chan bool, conn)
-	jobChan := make(chan string, conn)
-
-	signal.Notify(stopProducerChan, syscall.SIGINT, os.Interrupt)
-
-	var workerWG sync.WaitGroup
-	for i := 0; i < conn; i++ {
-		go worker(stopConsumerChan, jobChan, &workerWG)
+done:
+	for {
+		select {
+		case <-stop:
+			fmt.Println("Benchmark Stopping..")
+			break done
+		default:
+			tokens <- token{}
+			go work(url, tokens)
+		}
 	}
 
-	generate(url, stopProducerChan, jobChan)
-
-	for i := 0; i < conn; i++ {
-		stopConsumerChan <- true
+	// make sure existing goroutines have completed.
+	for i := 0; i < limit; i++ {
+		tokens <- token{}
 	}
-	workerWG.Wait()
 
-	close(stopProducerChan)
-	close(jobChan)
-	close(stopConsumerChan)
+	close(stop)
+	close(tokens)
 
 	fmt.Println("Benchmark Done")
 }
